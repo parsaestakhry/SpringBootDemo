9f88768ca0b1b9a52a417700a41ad4ebcf3195b6
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-24 20:48:38 +0330

    add @Component and @Primary annotations to Desktop class; retrieve Alien bean in main application

--- src/main/java/com/parsa/springbootdemo/Desktop.java
+++ src/main/java/com/parsa/springbootdemo/Desktop.java
@@ -1,5 +1,10 @@
 package com.parsa.springbootdemo;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class Desktop implements Computer {
 
 
--- src/main/java/com/parsa/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/parsa/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,17 @@ package com.parsa.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+        Alien alien = context.getBean(Alien.class);
+        alien.code();
+        System.out.println(alien.getAge());
     }
 
 }
